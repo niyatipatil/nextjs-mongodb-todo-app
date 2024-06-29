80369b8eae958f13eff10e9773d99afc23ded7d4
niyatipatil <patilniyati106@gmail.com>
2024-06-29 18:12:26 +0530

    commit

--- src/helper/db.js
+++ src/helper/db.js
@@ -10,16 +10,6 @@ export const connectDb = async () => {
     console.log("Connection to database Successful!");
     console.log("host : ", connection.host);
     //console.log(connection);
-
-    //testing - creating user new
-    /*const userr = new User({
-      name: "John Doe",
-      email: "john@example.com",
-      password: "password123",
-      about: "This is only for testing purpose",
-    });
-    await userr.save();
-    console.log("user is created");*/
   } catch (error) {
     console.log("Connection to database Failed!");
     console.log(error);
